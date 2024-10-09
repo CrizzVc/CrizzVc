d25ef0eb6f3c15e8bdafd76cf899ff4caf08aabb
CrizzVc <crizvc3@gmail.com>
2024-10-09 18:19:36 -0500

    app actualizada

--- Cristian_viana_camacho/apps/app_1/app/lib/main.dart
+++ Cristian_viana_camacho/apps/app_1/app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+// import 'ActividadAPI.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,31 +13,55 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         appBar: AppBar(
           title: Text('Json Place Holder'),
-          backgroundColor: Color.fromARGB(255, 180, 200, 255),
+          backgroundColor: Color.fromARGB(255, 35, 39, 49),
         ),
-        body: Column(
-          children: [
-            Text('ID: {user.id}'),
-            Text('NAME: {user.name}'),
-            Text('USERNAME: {user.username}'),
-            Text('email: {user.email}'),
-            // adress
-            Text('street: {user.street}'),
-            Text('suite: {user.suite}'),
-            Text('city: {user.city}'),
-            Text('zipcode: {user.zipcode}'),
-            // geo
-            Text('lat: {user.lat}'),
-            Text('lng: {user.lng}'),
+        body: Center(
+          child: Column(
+            children: [
+              Text('ID: {user.id}'),
+              Text('NAME: {user.name}'),
+              Text('USERNAME: {user.username}'),
+              Text('email: {user.email}'),
+              // adress
+              Text('street: {user.street}'),
+              Text('suite: {user.suite}'),
+              Text('city: {user.city}'),
+              Text('zipcode: {user.zipcode}'),
+              // geo
+              Text('lat: {user.lat}'),
+              Text('lng: {user.lng}'),
 
-            Text('phone: {user.phone}'),
-            Text('website: {user.website}'),
-            // 
-            Text('name: {user.name}'),
-            Text('cathPhrase: {user.cathPhrase}'),
-            Text('bs: {user.bs}'),
-            ElevatedButton(onPressed: (){}, child: Text('getData')),
-            CircularProgressIndicator()
+              Text('phone: {user.phone}'),
+              Text('website: {user.website}'),
+              // 
+              Text('name: {user.name}'),
+              Text('cathPhrase: {user.cathPhrase}'),
+              Text('bs: {user.bs}'),
+              ElevatedButton(onPressed: (){}, child: Text('getData')),
+              CircularProgressIndicator()
+            ],
+          ),
+        ),
+        bottomNavigationBar: GNav(
+          gap: 8,
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          tabs: [
+          GButton(
+            icon: Icons.home,
+            text: 'home',
+          ),
+          GButton(
+            icon: Icons.favorite_border,
+            text: 'likes',
+          ),
+          GButton(
+            icon: Icons.search,
+            text: 'search',
+          ),
+          GButton(
+            icon: Icons.settings,
+            text: 'settings',
+          ),
         ],)
       ),
     );
