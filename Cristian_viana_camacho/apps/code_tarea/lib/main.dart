37955a73adac5f2ba688e60f0cc70d01949dfda8
CrizzVc <crizvc3@gmail.com>
2024-10-19 00:13:19 -0500

    detalles

--- Cristian_viana_camacho/apps/code_tarea/lib/main.dart
+++ Cristian_viana_camacho/apps/code_tarea/lib/main.dart
@@ -1,6 +1,7 @@
 // import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:ninetythree_shadows/ninetythree_shadows.dart';
 // import 'ActividadAPI.dart';
 
 void main() {
@@ -79,71 +80,89 @@ class _MyHomePageState extends State<MyHomePage> {
           }else if (snapshot.hasError) {
             return Text('error: ${snapshot.error}');
           }else {
-            // int v1 = int.parse(text);
-            int v = 1;
             return Center(
-              child: Column(
+              child: Center( child:  Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  SizedBox(
-                    width: 265,
-                    child: TextField(
-                      controller: controller,
-                      onSubmitted: (String value) {
-                        setState(() {
-                          idu = int.tryParse(controller.text) ?? 1;
-                        });
+                  Container(
+                    width: 350,
+                    height: 450,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white,
+                      boxShadow: NTShadows.thirty,
+                    ),
+                    child: Column(
+                      children: [
+                        Text(' '),
+                        SizedBox(
+                          width: 265,
+                          child: TextField(
+                            controller: controller,
+                            onSubmitted: (String value) {
+                            setState(() {
+                            idu = int.tryParse(controller.text) ?? 1;
+                          }
+                        );
                       },
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'User id',
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'User id',
+                        ),
                       ),
                     ),
-                  ),
-                  Text(''),
-                  Text(
-                    'User id: $idu',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  Text(' '),
+                    Text(''),
+                    Text(
+                      'User id: $idu',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    Text(' '),
 
-                  Text(
-                    'Phone: ${data?[idu -1]['phone'] ?? '+57 301 ******1' }',
-                  ),
-                  Text(
-                    'User Name: ${data?[idu -1]['username'] ?? 'luchoPorTuAno'}',
-                  ),
-                  Text(
-                    'Nombre: ${data?[idu -1]['name'] ?? 'venito Camela'}',
-                  ),
-                  Text(
-                    'Email: ${data?[idu -1]['email'] ?? 'user@gmail.com'}',
-                  ),
-                  Text(
-                    ' ',
-                  ),
+                    Text(
+                      'Phone: ${data?[idu -1]['phone'] ?? '+57 301 ******1' }',
+                    ),
+                    Text(
+                      'User Name: ${data?[idu -1]['username'] ?? 'luchoPorTuAno'}',
+                    ),
+                    Text(
+                      'Nombre: ${data?[idu -1]['name'] ?? 'venito Camela'}',
+                    ),
+                    Text(
+                      'Email: ${data?[idu -1]['email'] ?? 'user@gmail.com'}',
+                    ),
+                    Text(
+                      ' ',
+                    ),
 
-                  Text(
-                    'address',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  Text(
-                    ' ',
-                  ),
+                    Text(
+                      'address',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    Text(
+                      ' ',
+                    ),
 
-                  Text(
-                    'Street: ${data?[idu -1]['address']['street'] ?? 'los tres postes'}',
+                    Text(
+                      'Street: ${data?[idu -1]['address']['street'] ?? 'los tres postes'}',
+                    ),
+                    Text(
+                      'Suite: ${data?[idu -1]['address']['suite'] ?? 'ap 2'}',
+                    ),
+                    Text(
+                      'City: ${data?[idu -1]['address']['city'] ?? 'Malambo'}',
+                    ),
+                        ],
+
+                      )
+
                   ),
-                  Text(
-                    'Suite: ${data?[idu -1]['address']['suite'] ?? 'ap 2'}',
-                  ),
-                  Text(
-                    'City: ${data?[idu -1]['address']['city'] ?? 'Malambo'}',
-                  ),
+                  
                 ],
               ),
+            ),
             );
+
           }
 
 
